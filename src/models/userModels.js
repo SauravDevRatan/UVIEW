e08ae412553ace557f23a891f6005f2e69a408a9
Sauravkumar <saurav23012001@gmail.com>
2025-10-30 11:26:38 +0530

    cludinary+multer

--- src/models/userModels.js
+++ src/models/userModels.js
@@ -26,8 +26,8 @@ userSchema.methods.isPasswordCorrect=async function(password){
     return await bcrypt.compare(password,this.password)
 }
 
-userSchema.methods.generateAccessToken= async function(){
-    return await jwt.sign(
+userSchema.methods.generateAccessToken=  function(){
+    return  jwt.sign(
         {
             _id:this._id,
         },
@@ -37,8 +37,8 @@ userSchema.methods.generateAccessToken= async function(){
         }
     )
 }
-userSchema.methods.refreshAccessToken=async function(){
-    return await jwt.sign(
+userSchema.methods.refreshAccessToken= function(){
+    return  jwt.sign(
         {
             _id:this._id,
             username:this.username,
