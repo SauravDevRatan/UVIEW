da7e9df499a00a241b15148bd7cd82716a793fcc
Sauravkumar <saurav23012001@gmail.com>
2025-10-30 08:30:38 +0530

    user and video models

--- src/app.js
+++ src/app.js
@@ -1,6 +1,18 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
 const app=express();
 
-
+app.use(cors(
+    {
+        origin:process.env.ORIGIN,
+        credentials:true
+    }
+))
+app.use(express.json({limit:"16kb"}));// to accept jason data
+app.use(express.urlencoded({extended:true,limit:"20kb"}));//to accept data caonning from url in proper format   extended true data inside data
+app.use(express.static("public"))//to explore data save usch as pdf images
+app.use(cookieParser());
 
 export {app};
